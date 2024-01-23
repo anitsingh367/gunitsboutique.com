e0ffb61dabf308269214caa456a1d5cf193d307e
Anit Singh <anitsingh367@gmail.com>
2024-01-23 22:41:01 +0530

    updated map, address and removed jd

--- main.js
+++ main.js
@@ -1218,11 +1218,11 @@ function sendback() {
   navlist.classList.remove("front");
 }
 
-function initMap() {
-  var uluru = { lat: 28.669178, lng: 77.1052347 };
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: uluru,
-  });
-  var marker = new google.maps.Marker({ position: uluru, map: map });
-}
+// function initMap() {
+//   var uluru = { lat: 28.669178, lng: 77.1052347 };
+//   var map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 15,
+//     center: uluru,
+//   });
+//   var marker = new google.maps.Marker({ position: uluru, map: map });
+// }
